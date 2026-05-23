156fbf7e4a1a4da5f3ffe014dca7bb21ca7d8cff
Himanshu Gupta <guptahimanshu102030@gmail.com>
2026-05-23 06:01:52 -0700

    Added practise_conditional.java

---
+++ practise_conditional.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class practise_conditional {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m1 = sc.nextInt();
+        System.out.println("enter the marks of first subject");
+        if(m1>33){
+            System.out.println("the student is pass");
+        }
+            else{
+                System.out.println("the studend is fail in first subject");
+        }
+        int m2 = sc.nextInt();
+        System.out.println("the studeent marks of second subject");
+    
+        if(m2>33){
+            System.out.println("the student is pass in second subkect");
+        }
+            else{
+                System.out.println("the sudent is fail second subject");
+            }
+        }
+    }
+
+    
+
